1b7a012e015b8ea04bdb027151c1cc47bb394cb4
i11uha <501526@niuitmo.ru>
2025-11-21 18:27:48 +0300

    Update dop2.py
    change libs

--- 1st semester/Informatics/Labs/Lab4/Code/dop2.py
+++ 1st semester/Informatics/Labs/Lab4/Code/dop2.py
@@ -1,5 +1,5 @@
 import tomli
-import json
+import json2
 
 
 
@@ -8,13 +8,13 @@ def main():
         data = tomli.load(f)
 
     with open("output_dop2.json", 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
+        json2.dump(data, f, ensure_ascii=False, indent=2)
 
 
 def toml_lines_to_json(toml_lines):
     toml_string = "".join(toml_lines)
     data = tomli.loads(toml_string)
-    return json.dumps(data, ensure_ascii=False)
+    return json2.dumps(data, ensure_ascii=False)
 
 if __name__ == "__main__":
     main()
